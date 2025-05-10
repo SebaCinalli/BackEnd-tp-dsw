3a14bb7dc2f66944f71f04c589c341f9279cfb53
Sebas <sebacinalli@gmail.com>
2025-05-10 10:45:04 -0300

    Corrige peticiones de cliente

--- src/cliente/cliente.routes.ts
+++ src/cliente/cliente.routes.ts
@@ -1,16 +1,21 @@
-import { Router } from "express";
-import {findAll, findById, add, modify, remove } from "./cliente.controller.js";
+import { Router } from 'express';
+import {
+  sanitizeClienteInput,
+  findAll,
+  findById,
+  add,
+  modify,
+  remove,
+} from './cliente.controller.js';
 
 export const ClienteRouter = Router();
 
+// Rutas de consulta no necesitan sanitización
 ClienteRouter.get('/', findAll);
-
 ClienteRouter.get('/:id', findById);
 
-ClienteRouter.post('/', add) 
-
-ClienteRouter.put('/:id', modify)
-
-ClienteRouter.patch('/:id', modify) 
-
+// Rutas que modifican datos necesitan sanitización
+ClienteRouter.post('/', sanitizeClienteInput, add);
+ClienteRouter.put('/:id', sanitizeClienteInput, modify);
+ClienteRouter.patch('/:id', sanitizeClienteInput, modify);
 ClienteRouter.delete('/:id', remove);
